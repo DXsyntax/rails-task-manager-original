16021a35a399c672baa380f9bfb660031a0e5f5c
Dexter Goh <bboifatt@gmail.com>
2020-03-10 14:01:24 +0800

    set up my show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'tasks', to: '/tasks#all' # did i do this correctly?
+get 'tasks', to: 'tasks#index'
+get 'tasks/:id', to: 'tasks#show' # raise a ticket to check if you did this correct
 end
